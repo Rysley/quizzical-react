b7004f229589a395615dae3f572df1be2471f50f
Rysley <ryszardleykam@gmail.com>
2022-11-11 13:29:50 +0100

    intitial Vite-boilerplate commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import Features from "./components/Features";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Features />
+    </div>
+  );
+}
+
+export default App;
