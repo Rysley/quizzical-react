fe6e80ccbd99d807a2da78ec3a4eba40f14ee573
Rysley <ryszardleykam@gmail.com>
2022-11-19 21:53:51 +0100

    carousel state handled

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -9,12 +9,41 @@ const style = {
 };
 
 export default function Welcome() {
+  const [formData, setFormData] = React.useState({
+    amount: 4,
+    difficulty: "easy",
+    category: "",
+    isSubmitted: false,
+  });
+
+  function logClick() {
+    console.log(formData);
+  }
+
+  function handleChange(event) {
+    //console.log(event);
+    const { id, name } = event.target;
+    console.log(id, name);
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: id,
+      };
+    });
+  }
+
   return (
-    <form className="welcome">
+    <section className="welcome">
       <h2 className="welcome__subtitle">
         Answer trivia questions from 25 fun categorties
       </h2>
-      <span className="welcome__menu-description">Select category</span>
+      <span
+        className="welcome__menu-description"
+        style={{ cursor: "pointer" }}
+        onClick={logClick}
+      >
+        Select category
+      </span>
       <div
         style={{
           display: "flex",
@@ -26,38 +55,44 @@ export default function Welcome() {
         <Carousel
           height={460}
           width={980}
-          yOrigin={10}
-          yRadius={70}
+          yOrigin={30}
+          yRadius={20}
           autoPlay={false}
         >
           <div key={1} style={style}>
             <span>ART</span>
             <img
               src={logo}
-              alt="react"
-              style={{ marginRight: "2rem" }}
-              onClick={console.log("click")}
+              alt="art"
+              id="art"
+              name="category"
+              onClick={(e) => handleChange(e)}
             />
           </div>
           <div key={2} style={style}>
             <span>SPORT</span>
-            <img src={logo} alt="react" style={{ marginRight: "2rem" }} />
+            <img
+              src={logo}
+              alt="sport"
+              id="sport"
+              name="category"
+              onClick={(e) => handleChange(e)}
+            />
           </div>
           <div key={3} style={style}>
             <span>SCIENCE</span>
-            <img src={logo} alt="react" style={{ marginRight: "2rem" }} />
-          </div>
-          <div key={4} style={style}>
-            <span>CINEMA</span>
-            <img src={logo} alt="react" style={{ marginRight: "2rem" }} />
-          </div>
-          <div key={5} style={style}>
-            <span>HISTORY</span>
-            <img src={logo} alt="react" style={{ marginRight: "2rem" }} />
+            <img
+              src={logo}
+              alt="science"
+              id="science"
+              name="category"
+              onClick={(e) => handleChange(e)}
+            />
           </div>
         </Carousel>
       </div>
+
       <button className="btn panel__new-game">Start quiz</button>
-    </form>
+    </section>
   );
 }
